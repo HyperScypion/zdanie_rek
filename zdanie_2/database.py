db1ba8b8d4ce4bf55ab2c30efc4cd1a6f31cb359
hyperscypion <tomasz_derek@wp.pl>
2020-11-21 17:45:10 +0100

    Added database, inserting images to databaset and findings max red, as well as getting most recent

--- zdanie_2/database.py
+++ zdanie_2/database.py
@@ -2,15 +2,10 @@ from pony import orm
 
 
 db = orm.Database()
-db.bind("sqlite", filename="database.sqlite", create_db=True)
 
 
 class ImageDB(db.Entity):
     id = orm.PrimaryKey(int, auto=True)
     name = orm.Required(str, unique=True)
     image = orm.Required(bytes, lazy=True)
-    red = orm.Optional(float)
-
-
-if __name__ == "__main__":
-    db.generate_mapping(create_tables=True)
+    red = orm.Required(float)
--- zdanie_2/main.py
+++ zdanie_2/main.py
@@ -1,14 +1,28 @@
 import io
+import json
 import flickrapi
 import argparse
+from database import db, ImageDB
+from PIL import Image
 import numpy as np
-
+import cv2
 from tqdm import tqdm
 import requests
+from pony.orm import *
 
 
-API_KEY = "1"
+API_KEY = ""
 API_SECRET = ""
+
+LOWER_RED_DOWN = np.array([0, 50, 50])
+UPPER_RED_DOWN = np.array([10, 255, 255])
+
+LOWER_RED_UP = np.array([170, 50, 50])
+UPPER_RED_UP = np.array([180, 255, 255])
+
+db.bind("sqlite", filename="database.sqlite", create_db=True)
+db.generate_mapping(create_tables=True)
+
 
 parser = argparse.ArgumentParser(description="Download images from Flickr")
 parser.add_argument("--keyword", type=str, help="Search for images based on keyword")
@@ -18,8 +32,22 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-def save_image(img_string):
-    image_array = np.array(io.BytesIO(img_string))
+def get_red(image):
+    image = np.array(Image.open(io.BytesIO(image)))
+    size = image.shape[0] * image.shape[1]
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask0 = cv2.inRange(image, LOWER_RED_DOWN, UPPER_RED_DOWN)
+    mask1 = cv2.inRange(image, LOWER_RED_UP, UPPER_RED_UP)
+    mask = mask0 + mask1
+    thresh = cv2.bitwise_and(image, image, mask=mask)
+    red = np.count_nonzero(thresh)
+    return round(red / size, 4)
+
+
+@db_session
+def get_max_red():
+    data = select((img, max(img.red)) for img in ImageDB)
+    print(data.first()[0][0])
 
 
 def enumerate_tqdm(y):
@@ -29,15 +57,20 @@ def enumerate_tqdm(y):
         i += 1
 
 
+@db_session
 def parse_images(photos, num=100):
     for idx, photo in enumerate_tqdm(photos):
         if idx == num:
             break
 
         url = photo.get("url_c")
+        if url is None:
+            continue
+        name = photo.get("title")
         image_string = requests.get(url).content
-        with open(f"{args.keyword}_{idx}.jpg", "wb") as fopen:
-            fopen.write(image_string)
+        red = get_red(image_string)
+        ImageDB(name=name, image=image_string, red=red)
+        db.commit()
 
 
 if __name__ == "__main__":
@@ -45,7 +78,27 @@ if __name__ == "__main__":
     if args.keyword is not None:
         if args.num is not None:
             photos = flick.walk(
-                text=args.keyword, tag_mode="all", extras="url_c", per_page=100
+                text=args.keyword,
+                tags=args.keyword,
+                tag_mode="all",
+                extras="url_c",
+                per_page=100,
             )
-            print("photos")
-            parse_images(photos)
+            parse_images(photos, args.num)
+        else:
+            print("Wrong usage exiting...")
+    else:
+        params = {
+            "api_key": API_KEY,
+            "extras": "url_c",
+            "per_page": 100,
+            "format": "json",
+        }
+
+        photos = requests.get(
+            f"https://www.flickr.com/services/rest/?method=flickr.photos.getRecent",
+            params=params,
+        ).content
+        print(photos)
+
+    get_max_red()
